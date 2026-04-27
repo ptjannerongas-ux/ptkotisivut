cfe182be12889ccab58b8da8949aa270de0979bf
ptjannerongas-ux <pt.jannerongas@gmail.com>
2026-04-27 16:50:47 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,6 @@
-const elements = document.querySelectorAll(".fade-in");
-
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if(entry.isIntersecting) {
-            entry.target.classList.add("visible");
-        }
-    });
-});
-
-elements.forEach(el => observer.observe(el));
-
 const toggle = document.getElementById("menu-toggle");
-const nav = document.querySelector("nav");
+const nav = document.getElementById("nav");
 
 toggle.addEventListener("click", () => {
     nav.classList.toggle("active");
-});
-
-document.querySelectorAll("nav a").forEach(link => {
-    link.addEventListener("click", () => {
-        nav.classList.remove("active");
-    });
 });
