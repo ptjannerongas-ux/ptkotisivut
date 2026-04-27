65e807732ff6c2a8f743d5734656311d63fe9446
ptjannerongas-ux <pt.jannerongas@gmail.com>
2026-04-27 21:22:11 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ const nav = document.getElementById("nav");
 toggle.addEventListener("click", () => {
     nav.classList.toggle("active");
 });
+
+window.addEventListener("scroll", () => {
+    document.querySelector("header").classList.toggle(
+        "scrolled",
+        window.scrollY > 50
+    );
+});
+
+const faders = document.querySelectorAll(".fade-up");
+
+const appear = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+faders.forEach(el => appear.observe(el));
