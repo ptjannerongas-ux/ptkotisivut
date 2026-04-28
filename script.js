cdaed8557bcc84a1a6bcaf42c6870c36816f223f
ptjannerongas-ux <pt.jannerongas@gmail.com>
2026-04-28 16:55:20 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
-const toggle = document.getElementById("menu-toggle");
-const nav = document.getElementById("nav");
+// QUIZ
+let current = 0;
+const steps = document.querySelectorAll(".step");
+const progress = document.querySelector(".progress");
 
-toggle.addEventListener("click", () => {
-    nav.classList.toggle("active");
-});
+function nextStep() {
+    steps[current].classList.remove("active");
+    current++;
 
-window.addEventListener("scroll", () => {
-    document.querySelector("header").classList.toggle(
-        "scrolled",
-        window.scrollY > 50
-    );
-});
+    if (current < steps.length) {
+        steps[current].classList.add("active");
+        progress.style.width = (current / (steps.length - 1)) * 100 + "%";
+    }
+}
 
+// ANIMATION
 const faders = document.querySelectorAll(".fade-up");
 
 const appear = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-        if(entry.isIntersecting){
+        if (entry.isIntersecting) {
             entry.target.classList.add("show");
         }
     });
 });
 
 faders.forEach(el => appear.observe(el));
-
-let current = 0;
-const steps = document.querySelectorAll(".step");
-
-function nextStep() {
-    steps[current].classList.remove("active");
-    current++;
-    steps[current].classList.add("active");
-}
